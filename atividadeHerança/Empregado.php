d8e64080049e8b342f423435ebbe9ea48076860f
Dodoh04 <douglasoliversa3@gmail.com>
2023-04-03 22:11:33 -0300

    parece estar andando

--- atividadeHerança/Empregado.php
+++ atividadeHerança/Empregado.php
@@ -5,14 +5,13 @@
         var $sobrenome;
         var $cpf;
 
+        
          function __construct($nome, $sobrenome, $cpf)
         {
             $this->nome = $nome;
             $this->sobrenome = $sobrenome;
             $this->$cpf = $cpf;
-            $this->nascimento = $nascimento; 
-            $this->escolaridade = $escolaridade; 
-            $this->salario = $salario; 
+
          }
          //metodo contruct 
          function __destruct()
