d9d4417fd868fd723767c3b5547a5d42d82196d6
yphill <phelipequeiroz2004@gmail.com>
2023-04-10 21:59:40 -0300

    criando metodos ir e inss em classe pai

--- atividadeHerança/Horista.php
+++ atividadeHerança/Horista.php
@@ -11,28 +11,10 @@
             $this->horasTrabalhadas = $horasTrabalhadas;
 
         }         
-        function Vencimento ($quantia)
+        function Vencimento()
         {
-            $quantia = $this->precoHora * $this->horasTrabalhadas;
-            
-            if($quantia <= 1302.00 ){
-               $inss = 0.075;
-               $quantia -= $inss;
-            }
-            if($quantia <= 2571.29){
-                $inss = 0.09;
-               $quantia -= $inss;
-            }
-            if($quantia <= 3000.00){
-                $inss = 0.12;
-                $quantia -= $inss;
-            }
-            else{
-                $inss = 263.33;
-                $quantia -= $inss;
-            }
+           return $this->precoHora * $this-> horasTrabalhadas;
         }
-        
         
     }
     
